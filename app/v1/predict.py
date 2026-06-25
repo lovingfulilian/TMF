eef76dc4974283692f2702a4f2b47b3f62ccec68
jackfrued <jackfrued@126.com>
2026-06-25 22:48:51 +0800

    修复了代码中存在的问题

--- app/v1/predict.py
+++ app/v1/predict.py
@@ -4,7 +4,7 @@ predict - API 接口实现
 Author: 骆昊
 Version: 0.0.1
 """
-from flask import Blueprint, g, request
+from flask import Blueprint, request
 from app.extensions import thy_extension
 from src.data_pre import cut_zh_words
 
@@ -12,20 +12,26 @@ from src.data_pre import cut_zh_words
 model_bp = Blueprint('models', __name__, url_prefix='/api/v1')
 
 
-@model_bp.before_request
-def before_request():
-    """每个请求进来时把启动时加载好的模型单例安全地绑定到当前请求的 g 对象上"""
-    g.model = thy_extension.text_clf_model
-    g.class_labels = thy_extension.class_labels
+# @model_bp.before_request
+# def before_request():
+#     """每个请求进来时把启动时加载好的模型单例安全地绑定到当前请求的 g 对象上"""
+#     g.model = thy_extension.text_clf_model
+#     g.class_labels = thy_extension.class_labels
 
 
 @model_bp.route('/predict', methods=['GET', 'POST'])
 def text_clf_predict():
     """文本分类预测"""
-    json_string = request.get_json()
+    # silent=True - 如果解析 JSON 失败不抛出异常而是返回 None
+    # force=True - 强制将消息体解析为 JSON 格式
+    json_string = request.get_json(silent=True, force=True)
+    if not json_string:
+        return {'code': -20, 'message': '请求体不符合 JSON 格式规范'}
+
     text = json_string.get('text', '')
-    if text:
-        text = cut_zh_words(text)
-        y_pred = g.model.predict([text])
-        return {'label': g.class_labels[y_pred[0]], 'code': 0, 'message': 'OK'}
-    return {'code': -10, 'message': '请提供要分类的文本内容'}
+    if not text:
+        return {'code': -10, 'message': '请提供要分类的文本内容'}
+
+    text = cut_zh_words(text)
+    y_pred = thy_extension.text_clf_model.predict([text])
+    return {'code': 0, 'message': 'OK', 'label': thy_extension.class_labels[y_pred[0]]}
---
+++ front/tmf_app.py
@@ -0,0 +1,57 @@
+"""
+tmf_app - Streamlit 用户界面
+
+Author: 骆昊
+Version: 0.0.1
+"""
+import sys
+from pathlib import Path
+
+import requests
+import streamlit as st
+from loguru import logger
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_URL = "http://127.0.0.1:8080"
+
+logger.remove()
+logger.add(sys.stderr, level='INFO')
+logger.add(
+    BASE_DIR / 'logs/api_service.log',
+    rotation='100 MB',   # 何时关闭当前日志文件并创建新文件
+    retention='7 days',  # 旧日志文件的回收与清理策略
+    serialize=True,      # 是否将日志转换为 JSON 字符串输出
+    level='INFO',        # 日志处理器接收的最低日志级别
+    enqueue=True,        # 日志打印非阻塞
+    catch=True,          # 防止日志写入失败导致应用崩溃
+    compression='zip',   # 当日志触发切分时旧的日志文件会被自动压缩
+)
+
+
+def get_class_label(text):
+    """获取类别标签"""
+    try:
+        resp = requests.get(
+            url=f'{BASE_URL}/api/v1/predict',
+            headers={'content-type': 'application/json'},
+            json={'text': text},
+        )
+        result = resp.json()
+        if result['code'] == 0:
+            return result['label']
+        return result['message']
+    except Exception as e:
+        logger.error(str(e))
+        return ''
+
+
+st.write('## 投满分（文本分类专家）')
+content = st.text_input(label='请输入要分类的文本内容：')
+ok_button = st.button('确定')
+
+if ok_button and content.strip():
+    class_label = get_class_label(content)
+    if class_label:
+        st.write(f'**分类结果**：{class_label}')
+    else:
+        st.error('服务器维护中，请稍后再尝试访问')
